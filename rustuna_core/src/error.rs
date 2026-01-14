aea2c49197692c458c44c2e420d20f32f0e560be
c-bata <contact@c-bata.link>
2026-01-14 13:46:27 +0900

    Add reason and location in Error struct

--- rustuna_core/src/error.rs
+++ rustuna_core/src/error.rs
@@ -1,16 +1,49 @@
+use std::panic::Location;
+
 /// This crate specific `Error` type.
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct Error {
     pub kind: ErrorKind,
+    pub reason: String,
+    pub location: &'static Location<'static>,
 }
 impl Error {
+    #[track_caller]
     pub fn new(kind: ErrorKind) -> Self {
-        Error { kind }
+        Self::with_reason(kind, String::new())
+    }
+
+    #[track_caller]
+    pub fn with_reason<T: Into<String>>(kind: ErrorKind, reason: T) -> Self {
+        Self {
+            kind,
+            reason: reason.into(),
+            location: Location::caller(),
+        }
+    }
+}
+
+impl core::fmt::Debug for Error {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "{self}")
+    }
+}
+
+impl core::fmt::Display for Error {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        if self.reason.is_empty() {
+            write!(f, "{:?}", self.kind)?;
+        } else {
+            write!(f, "{:?}: {}", self.kind, self.reason)?;
+        }
+        write!(f, " (at {}:{})", self.location.file(), self.location.line())?;
+        Ok(())
     }
 }
 
 /// ErrorKind represents the kind of error.
 #[derive(Debug, Clone)]
+#[non_exhaustive]
 pub enum ErrorKind {
     ObjectiveError,
     SamplerError,
