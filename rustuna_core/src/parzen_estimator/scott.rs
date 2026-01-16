d6f10f139bb5c5518f7c7226fa7c52a3a7b46833
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2026-01-16 19:09:40 +0900

    Impl ScottNumericalDistributionBuilder

--- rustuna_core/src/parzen_estimator/scott.rs
+++ rustuna_core/src/parzen_estimator/scott.rs
@@ -3,7 +3,30 @@ use crate::parzen_estimator::model::NumericalDistributionBuilder;
 use crate::distribution::Distribution;
 
 
-
 pub struct ScottNumericalDistributionBuilder {
     weights: &[f64],
+}
+
+impl NumericalDistributionBuilder for ScottNumericalDistributionBuilder {
+    fn calculate_numerical_distribution(
+        &self,
+        observations: &[f64],
+        search_space: &Distribution::Int,
+    ) -> Distributions {
+        let (low, high, step, log) = (
+            search_space.low as f64,
+            search_space.high as f64,
+            search_space.step as f64,
+            search_space.log,
+        );
+        let weights_sum = self.weights.iter().sum();
+        
+        let mean_est = observations.iter().zip(self.weights.iter()).map(|(v, w)| v * w / weights_sum);
+        let sigma_est = {
+            let var = observations.iter().zip(mean_est).zip(self.weights.iter())
+            .map(|((v, m), w)| w * (v - m).powi(2)).sum() / (weights_sum - 1.0).max(1.0);
+            var.sqrt()
+        };
+        
+    }
 }
