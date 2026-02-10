23a49e76d4241358c7271ffad65731b523f498a6
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2026-02-10 15:34:26 +0900

    Update lib.rs

--- rustuna_importance/src/lib.rs
+++ rustuna_importance/src/lib.rs
@@ -1,1 +1,6 @@
 pub mod fanova;
+mod ped_anova;
+mod common;
+
+pub use ped_anova::PedAnovaImportanceEvaluator;
+pub use common::{get_param_importances, get_param_importances_with, ImportanceEvaluator, ImportanceOptions};
