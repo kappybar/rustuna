8318c707804f9a21188587b6334d316907e240f6
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2026-07-15 19:20:15 +0900

    Update py_get_param_importances

--- rustuna_pyo3/src/importance.rs
+++ rustuna_pyo3/src/importance.rs
@@ -10,15 +10,16 @@ use crate::study::PyStudy;
 
 #[pyfunction]
 #[pyo3(name = "get_param_importances", signature = (study, *, evaluator = None, params = None, normalize = true))]
-pub fn py_get_param_importances(study: &PyStudy, evaluator: Option<PyPedAnovaImportanceEvaluator>, params: Option<Vec<String>>, normalize: bool) -> PyResult<HashMap<String,f64>> {
-    let opts = ImportanceOptions {
+pub fn py_get_param_importances(study: &PyStudy, evaluator: Option<&PyPedAnovaImportanceEvaluator>, params: Option<Vec<String>>, normalize: bool) -> PyResult<HashMap<String,f64>> {
+    let options = ImportanceOptions {
         target: None,
         normalize,
         params,
     };
-    let evaluator = evaluator.map(|wrapper| wrapper.evaluator).unwrap_or(PedAnovaImportanceEvaluator::default());
-    let importances = get_param_importances_with(&study.study, &evaluator, opts)
-        .map_err(|err| PyRuntimeError::new_err(format!("Failed to get parameter importances: {err:?}")))?;
+    let default_evaluator = PedAnovaImportanceEvaluator::default();
+    let evaluator = evaluator.map(|wrapper| &wrapper.evaluator).unwrap_or(&default_evaluator);
+    let importances = get_param_importances_with(&study.study, evaluator, options)
+        .map_err(|err| PyValueError::new_err(err.reason))?;
     Ok(importances)
 }
 
