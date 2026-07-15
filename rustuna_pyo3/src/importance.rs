baad17a9a71b32651ef0890647b1bbc75ae8f69f
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2026-07-15 17:52:17 +0900

    Update py_get_param_importances

--- rustuna_pyo3/src/importance.rs
+++ rustuna_pyo3/src/importance.rs
@@ -7,9 +7,14 @@ use rustuna_importance::fanova::get_param_importance;
 use crate::study::PyStudy;
 
 #[pyfunction]
-#[pyo3(name = "get_param_importance", signature = (study))]
-pub fn py_get_param_importance(study: &PyStudy) -> PyResult<Vec<Vec<f64>>> {
-    let importance = get_param_importance(&study.study)
+#[pyo3(name = "get_param_importances", signature = (study, *, evaluator = None, params = None, normalize = true))]
+pub fn py_get_param_importances(study: &PyStudy, evaluator: Option<PedAnovaImportanceEvaluator>, params: Option<Vec<String>>, normalize: bool) -> PyResult<HashMap<String,f64>> {
+    let opts = ImportanceOptions {
+        target: None,
+        normalize,
+        params,
+    };
+    let importance = get_param_importances_with(&study.study, &evaluator.unwrap_or(PedAnovaImportanceEvaluator::default()), opts)
         .map_err(|_e| PyRuntimeError::new_err("Failed to get parameter importance"))?;
     Ok(importance)
 }
