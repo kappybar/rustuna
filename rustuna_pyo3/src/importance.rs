a4de37d66ad0b02f60cda84fc4922cdc3b946988
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2026-07-15 19:22:18 +0900

    Apply formatter

--- rustuna_pyo3/src/importance.rs
+++ rustuna_pyo3/src/importance.rs
@@ -1,23 +1,32 @@
 use std::collections::HashMap;
 
-use pyo3::exceptions::{PyValueError, PyUserWarning};
+use pyo3::exceptions::{PyUserWarning, PyValueError};
 use pyo3::prelude::*;
 use pyo3::PyResult;
 
-use rustuna_importance::{get_param_importances_with, ImportanceOptions, PedAnovaImportanceEvaluator};
+use rustuna_importance::{
+    get_param_importances_with, ImportanceOptions, PedAnovaImportanceEvaluator,
+};
 
 use crate::study::PyStudy;
 
 #[pyfunction]
 #[pyo3(name = "get_param_importances", signature = (study, *, evaluator = None, params = None, normalize = true))]
-pub fn py_get_param_importances(study: &PyStudy, evaluator: Option<&PyPedAnovaImportanceEvaluator>, params: Option<Vec<String>>, normalize: bool) -> PyResult<HashMap<String,f64>> {
+pub fn py_get_param_importances(
+    study: &PyStudy,
+    evaluator: Option<&PyPedAnovaImportanceEvaluator>,
+    params: Option<Vec<String>>,
+    normalize: bool,
+) -> PyResult<HashMap<String, f64>> {
     let options = ImportanceOptions {
         target: None,
         normalize,
         params,
     };
     let default_evaluator = PedAnovaImportanceEvaluator::default();
-    let evaluator = evaluator.map(|wrapper| &wrapper.evaluator).unwrap_or(&default_evaluator);
+    let evaluator = evaluator
+        .map(|wrapper| &wrapper.evaluator)
+        .unwrap_or(&default_evaluator);
     let importances = get_param_importances_with(&study.study, evaluator, options)
         .map_err(|err| PyValueError::new_err(err.reason))?;
     Ok(importances)
@@ -33,20 +42,25 @@ pub struct PyPedAnovaImportanceEvaluator {
 impl PyPedAnovaImportanceEvaluator {
     #[new]
     #[pyo3(signature = (target_quantile = 0.1, region_quantile = 1.0, evaluate_on_local = true))]
-    fn py_new(py: Python<'_>, target_quantile: f64, region_quantile: f64, evaluate_on_local: bool) -> PyResult<Self> {
-        let evaluator = PedAnovaImportanceEvaluator::new(target_quantile, region_quantile, evaluate_on_local)
-            .map_err(|err| PyValueError::new_err(err.reason))?;
+    fn py_new(
+        py: Python<'_>,
+        target_quantile: f64,
+        region_quantile: f64,
+        evaluate_on_local: bool,
+    ) -> PyResult<Self> {
+        let evaluator =
+            PedAnovaImportanceEvaluator::new(target_quantile, region_quantile, evaluate_on_local)
+                .map_err(|err| PyValueError::new_err(err.reason))?;
         if region_quantile != 1.0 && evaluate_on_local {
             PyErr::warn(
                 py,
                 &py.get_type::<PyUserWarning>(),
-                pyo3::ffi::c_str!("If `evaluate_on_local` is False, `region_quantile` has no effect."),
+                pyo3::ffi::c_str!(
+                    "If `evaluate_on_local` is False, `region_quantile` has no effect."
+                ),
                 1,
             )?;
         }
-        Ok(Self {
-            evaluator,
-
-        })
+        Ok(Self { evaluator })
     }
 }
