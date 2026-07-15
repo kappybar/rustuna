a2065a0fdcc475cdc84733070c8f07ade980e805
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2026-07-15 18:51:11 +0900

    Update use

--- rustuna_importance/src/ped_anova.rs
+++ rustuna_importance/src/ped_anova.rs
@@ -4,6 +4,7 @@ use rustuna_core::parzen_estimator::ParzenEstimator;
 use rustuna_core::study::{Direction, Study};
 use rustuna_core::trial::PersistedTrial;
 use rustuna_core::Result;
+use rustuna_core::{Error, ErrorKind};
 use std::collections::{BTreeSet, HashMap, HashSet};
 
 /// PED-ANOVA importance evaluator.
