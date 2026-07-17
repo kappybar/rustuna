649860bcd0b28019e2f50688eae4a12f437be365
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2026-07-17 20:07:35 +0900

    Use Distribution constructors

--- rustuna_core/src/distribution.rs
+++ rustuna_core/src/distribution.rs
@@ -225,19 +225,9 @@ mod tests {
 
     #[test]
     fn test_check_compatibility_different() {
-        let f = Distribution::Float {
-            low: 0.0,
-            high: 1.0,
-            step: None,
-            log: false,
-        };
-        let i = Distribution::Int {
-            low: 0,
-            high: 1,
-            step: 1,
-            log: false,
-        };
-        let c = Distribution::Categorical { cardinality: 3 };
+        let f = Distribution::new_float(0.0, 1.0, None, false);
+        let i = Distribution::new_int(0, 1, 1, false);
+        let c = Distribution::new_categorical(3);
         assert!(f.check_compatibility(&i).is_err());
         assert!(f.check_compatibility(&c).is_err());
         assert!(i.check_compatibility(&f).is_err());
@@ -248,60 +238,30 @@ mod tests {
 
     #[test]
     fn test_check_compatibility_float() {
-        let f1 = Distribution::Float {
-            low: 0.0,
-            high: 1.0,
-            step: None,
-            log: false,
-        };
-        let f2 = Distribution::Float {
-            low: -1.0,
-            high: 2.0,
-            step: None,
-            log: false,
-        };
+        let f1 = Distribution::new_float(0.0, 1.0, None, false);
+        let f2 = Distribution::new_float(-1.0, 2.0, None, false);
         assert!(f1.check_compatibility(&f1).is_ok());
         assert!(f1.check_compatibility(&f2).is_ok());
 
-        let f1_log = Distribution::Float {
-            low: 0.0,
-            high: 1.0,
-            step: None,
-            log: true,
-        };
+        let f1_log = Distribution::new_float(0.0, 1.0, None, true);
         assert!(f1.check_compatibility(&f1_log).is_err());
     }
 
     #[test]
     fn test_check_compatibility_int() {
-        let i1 = Distribution::Int {
-            low: 0,
-            high: 1,
-            step: 1,
-            log: false,
-        };
-        let i2 = Distribution::Int {
-            low: 0,
-            high: 2,
-            step: 1,
-            log: false,
-        };
+        let i1 = Distribution::new_int(0, 1, 1, false);
+        let i2 = Distribution::new_int(0, 2, 1, false);
         assert!(i1.check_compatibility(&i1).is_ok());
         assert!(i1.check_compatibility(&i2).is_ok());
 
-        let i1_log = Distribution::Int {
-            low: 0,
-            high: 1,
-            step: 1,
-            log: true,
-        };
+        let i1_log = Distribution::new_int(0, 1, 1, true);
         assert!(i1.check_compatibility(&i1_log).is_err());
     }
 
     #[test]
     fn test_check_compatibility_categorical() {
-        let c1 = Distribution::Categorical { cardinality: 3 };
-        let c2 = Distribution::Categorical { cardinality: 4 };
+        let c1 = Distribution::new_categorical(3);
+        let c2 = Distribution::new_categorical(4);
         assert!(c1.check_compatibility(&c1).is_ok());
         assert!(c1.check_compatibility(&c2).is_err());
     }
