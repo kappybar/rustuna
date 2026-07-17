6c0817972769c599f83d908b005abe69dfe2726e
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2026-07-17 20:20:04 +0900

    Use Distribution constructors

--- rustuna_importance/src/test_utils.rs
+++ rustuna_importance/src/test_utils.rs
@@ -13,68 +13,20 @@ pub(crate) enum ObjectiveType {
 
 fn single_objective(mut trial: Trial) -> Result<Vec<f64>> {
     let x1 = trial.suggest_float("x1", 0.1, 3.0)?;
-    let x2 = trial.suggest(
-        "x2",
-        &Distribution::Float {
-            low: 0.1,
-            high: 0.3,
-            step: None,
-            log: true,
-        },
-    )?;
-    let x3 = trial.suggest(
-        "x3",
-        &Distribution::Float {
-            low: 1.0,
-            high: 3.0,
-            step: Some(1.0),
-            log: true,
-        },
-    )?;
+    let x2 = trial.suggest("x2", &Distribution::new_float(0.1, 0.3, None, true))?;
+    let x3 = trial.suggest("x3", &Distribution::new_float(1.0, 3.0, Some(1.0), true))?;
     let x4 = trial.suggest_int("x4", -3, 3)?;
-    let x5 = trial.suggest(
-        "x5",
-        &Distribution::Int {
-            low: 1,
-            high: 3,
-            step: 1,
-            log: true,
-        },
-    )?;
+    let x5 = trial.suggest("x5", &Distribution::new_int(1, 3, 1, true))?;
     let x6 = trial.suggest_categorical("x6", &[1.0, 1.1, 1.2])?;
     Ok(vec![x1.powi(4) + x2 + x3 - x4.pow(2) as f64 - x5 + x6])
 }
 
 fn multi_objective(mut trial: Trial) -> Result<Vec<f64>> {
     let x1 = trial.suggest_float("x1", 0.1, 3.0)?;
-    let x2 = trial.suggest(
-        "x2",
-        &Distribution::Float {
-            low: 0.1,
-            high: 0.3,
-            step: None,
-            log: true,
-        },
-    )?;
-    let x3 = trial.suggest(
-        "x3",
-        &Distribution::Float {
-            low: 0.1,
-            high: 3.0,
-            step: Some(1.0),
-            log: true,
-        },
-    )?;
+    let x2 = trial.suggest("x2", &Distribution::new_float(0.1, 0.3, None, true))?;
+    let x3 = trial.suggest("x3", &Distribution::new_float(0.1, 3.0, Some(1.0), true))?;
     let x4 = trial.suggest_int("x4", -3, 3)?;
-    let x5 = trial.suggest(
-        "x5",
-        &Distribution::Int {
-            low: 1,
-            high: 3,
-            step: 1,
-            log: true,
-        },
-    )?;
+    let x5 = trial.suggest("x5", &Distribution::new_int(1, 3, 1, true))?;
     let x6 = trial.suggest_categorical("x6", &[1.0, 1.1, 1.2])?;
     Ok(vec![x1.powi(4) + x2 + x3, x4.pow(2) as f64 - x5 + x6])
 }
