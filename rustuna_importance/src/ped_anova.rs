c8804e1d5c9c761368739b15df97c370df4d2c2d
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2026-07-24 17:48:55 +0900

    Remove min_n_top_trials

--- rustuna_importance/src/ped_anova.rs
+++ rustuna_importance/src/ped_anova.rs
@@ -136,15 +136,9 @@ impl PedAnovaImportanceEvaluator {
             })
             .collect::<Vec<_>>();
         let num_top_trials = ((quantile * trials.len() as f64).ceil() as usize).max(1) - 1;
-        let threshold = {
-            let (_, &mut threshold, _) = objective_values
-                .clone()
-                .select_nth_unstable_by(num_top_trials, |a, b| a.total_cmp(b));
-            let (_, &mut threshold_min, _) = objective_values
-                .clone()
-                .select_nth_unstable_by(self.min_n_top_trials - 1, |a, b| a.total_cmp(b));
-            threshold.max(threshold_min)
-        };
+        let (_, &mut threshold, _) = objective_values
+            .clone()
+            .select_nth_unstable_by(num_top_trials, |a, b| a.total_cmp(b));
         let top_trials = trials
             .iter()
             .zip(objective_values.iter())
