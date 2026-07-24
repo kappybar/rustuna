d2dc77efa9e2d516d9af4112473c398ef205898e
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2026-07-24 20:10:18 +0900

    Add test_importance

---
+++ rustuna_pyo3/tests/test_importance.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from collections.abc import Callable
+
+import pytest
+from _pytest.fixtures import SubRequest
+from optuna.importance import BaseImportanceEvaluator
+from optuna.testing.pytest_importance import (
+    BasicImportanceEvaluatorTestCase,
+    ConditionalImportanceEvaluatorTestCase,
+)
+
+from rustuna.converter import ToOptunaImportanceEvaluator
+from rustuna.importance import PedAnovaImportanceEvaluator
+
+
+class TestBasicImportanceEvaluator(BasicImportanceEvaluatorTestCase):
+    @pytest.fixture(params=[PedAnovaImportanceEvaluator])
+    def evaluator(self, request: SubRequest) -> Callable[..., BaseImportanceEvaluator]:
+        return lambda: ToOptunaImportanceEvaluator(request.param())
+
+
+class TestConditionalImportanceEvaluator(ConditionalImportanceEvaluatorTestCase):
+    @pytest.fixture(params=[PedAnovaImportanceEvaluator])
+    def evaluator(self, request: SubRequest) -> Callable[..., BaseImportanceEvaluator]:
+        return lambda: ToOptunaImportanceEvaluator(request.param())
