1b015c1d511093680e8697d19190a4241efe6063
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2026-07-24 20:08:44 +0900

    Add ToOptunaImportanceEvaluator

---
+++ rustuna_pyo3/rustuna/converter/_importance.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from collections.abc import Callable
+
+import optuna
+from optuna.importance import BaseImportanceEvaluator
+from optuna.trial import FrozenTrial
+
+import rustuna
+from rustuna.converter._storage import ToRustunaStorage
+
+
+class ToOptunaImportanceEvaluator(BaseImportanceEvaluator):
+    def __init__(
+        self,
+        evaluator: rustuna.importance.PedAnovaImportanceEvaluator,
+    ) -> None:
+        self._evaluator = evaluator
+
+    def evaluate(
+        self,
+        study: optuna.Study,
+        params: list[str] | None = None,
+        *,
+        target: Callable[[FrozenTrial], float] | None = None,
+    ) -> dict[str, float]:
+        rustuna_study = rustuna.load_study(
+            study.study_name,
+            ToRustunaStorage(study._storage),
+        )
+
+        return self._evaluator.evaluate(
+            rustuna_study,
+            params=params
+        )
